a34c79d0eb2c7af0b4a037a53360ac3678702ad3
steelfire81 <wcrobbins@loyola.edu>
2019-12-11 20:44:04 -0500

    Cleaned up common triangle formation solution

--- MeshRoadUtil.cs
+++ MeshRoadUtil.cs
@@ -8,62 +8,99 @@ using UnityEngine;
 public static class MeshRoadUtil
 {
     /// <summary>
+    /// Set of triangles forming a standard mesh.
+    /// </summary>
+    private class TriangleSet
+    {
+        // DATA MEMBERS
+        /// <summary>
+        /// Number of the current triangle being modified.
+        /// </summary>
+        private int currentTriangle;
+
+        /// <summary>
+        /// Array of vertices, each set of 3 forming a triangle in the mesh.
+        /// </summary>
+        private int[] trianglePoints;
+
+        // FUNCTIONS
+        /// <summary>
+        /// Initialize a triangle set.
+        /// </summary>
+        /// <param name="numTriangles">Maximum number of triangles in the set.</param>
+        public TriangleSet(int numTriangles)
+        {
+            currentTriangle = 0;
+            trianglePoints = new int[numTriangles * 3];
+        }
+
+        /// <summary>
+        /// Add 2 triangles to this set to form a given rectangle.
+        /// </summary>
+        /// <param name="vertA">Index of top left vertex.</param>
+        /// <param name="vertB">Index of top right vertex.</param>
+        /// <param name="vertC">Index of bottom left vertex.</param>
+        /// <param name="vertD">Index of bottom right vertex.</param>
+        public void pushTriangles(int vertA, int vertB, int vertC, int vertD)
+        {
+            int tOffset = currentTriangle * 3;
+
+            trianglePoints[tOffset] = vertA;
+            trianglePoints[tOffset + 1] = vertB;
+            trianglePoints[tOffset + 2] = vertC;
+
+            trianglePoints[tOffset + 3] = vertC;
+            trianglePoints[tOffset + 4] = vertD;
+            trianglePoints[tOffset + 5] = vertA;
+
+            currentTriangle += 2;
+        }
+
+        /// <summary>
+        /// Get the triangle point array from this set of triangles.
+        /// </summary>
+        /// <returns>Triangle point array from this set of triangles.</returns>
+        public int[] getTrianglePoints()
+        {
+            return trianglePoints;
+        }
+    }
+
+    // FUNCTIONS
+    /// <summary>
     /// Generate triangle array for a standard mesh consisting of rectangular cross sections.
     /// </summary>
     /// <param name="numCrossSections">Number of rectangular cross sections "framing" the mesh.</param>
     /// <returns>Array of integers, each set of 3 representing a triangle in the mesh.</returns>
     public static int[] getStandardMeshTriangles(int numCrossSections)
     {
-        int[] triangles = new int[((numCrossSections - 1) * 8 + 4) * 3];
         int numVertices = numCrossSections * 4;
+        TriangleSet triangleSet = new TriangleSet((numCrossSections - 1) * 8 + 4);
 
         // Form appropriate triangles for each set of two cross sections
         for (int i = 0; i < numCrossSections - 1; i++)
         {
-            int tOffset = i * 8 * 3;
             int vOffset = i * 4;
 
             // Top triangles
-            pushTriangles(ref triangles, tOffset, vOffset, vOffset + 4, vOffset + 5, vOffset + 1);
+            triangleSet.pushTriangles(vOffset, vOffset + 4, vOffset + 5, vOffset + 1);
 
             // Bottom triangles
-            pushTriangles(ref triangles, tOffset + 6, vOffset + 2, vOffset + 3, vOffset + 7, vOffset + 6);
+            triangleSet.pushTriangles(vOffset + 2, vOffset + 3, vOffset + 7, vOffset + 6);
 
             // Left triangles
-            pushTriangles(ref triangles, tOffset + 12, vOffset, vOffset + 2, vOffset + 6, vOffset + 4);
+            triangleSet.pushTriangles(vOffset, vOffset + 2, vOffset + 6, vOffset + 4);
 
             // Right triangles
-            pushTriangles(ref triangles, tOffset + 18, vOffset + 1, vOffset + 5, vOffset + 7, vOffset + 3);
+            triangleSet.pushTriangles(vOffset + 1, vOffset + 5, vOffset + 7, vOffset + 3);
         }
 
         // Special case: front triangles on first cross section
-        pushTriangles(ref triangles, triangles.Length - 12, 0, 1, 3, 2);
+        triangleSet.pushTriangles(0, 1, 3, 2);
 
         // Special case: rear triangles on last cross section
-        pushTriangles(ref triangles, triangles.Length - 6, numVertices - 4, numVertices - 2,
-            numVertices - 1, numVertices - 3);
+        triangleSet.pushTriangles(numVertices - 4, numVertices - 2, numVertices - 1, numVertices - 3);
 
-        return triangles;
-    }
-
-    /// <summary>
-    /// Create forward-facing triangles from a rectangle and insert them into the list of triangles.
-    /// 
-    /// "Front" side of the rectangle is formed by provided vertices in clockwise order.
-    /// </summary>
-    /// <param name="tOffset">Index at which to insert the first triangle.</param>
-    /// <param name="vertA">Index of top left vertex.</param>
-    /// <param name="vertB">Index of top right vertex.</param>
-    /// <param name="vertC">Index of bottom left vertex.</param>
-    /// <param name="vertD">Index of bottom right vertex.</param>
-    private static void pushTriangles(ref int[] triangles, int tOffset, int vertA, int vertB, int vertC, int vertD)
-    {
-        triangles[tOffset] = vertA;
-        triangles[tOffset + 1] = vertB;
-        triangles[tOffset + 2] = vertC;
-
-        triangles[tOffset + 3] = vertC;
-        triangles[tOffset + 4] = vertD;
-        triangles[tOffset + 5] = vertA;
+        return triangleSet.getTrianglePoints();
     }
 }
